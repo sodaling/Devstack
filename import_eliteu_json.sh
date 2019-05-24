4130c08de649dfcbdad29023a82225e082f79643
sodaling <502464760@qq.com>
2019-05-24 10:52:54 +0800

    fix error

--- import_eliteu_json.sh
+++ import_eliteu_json.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-docker cp ~/Downloads/lms.auth.json.json edx.devstack.studio:/edx/app/edxapp/
-docker cp ~/Downloads/lms.env.json.json edx.devstack.studio:/edx/app/edxapp/
-docker cp ~/Downloads/cms.auth.json.json edx.devstack.studio:/edx/app/edxapp/
-docker cp ~/Downloads/lms.env.json.json edx.devstack.studio:/edx/app/edxapp/
+docker cp ~/Downloads/lms.auth.json edx.devstack.studio:/edx/app/edxapp/
+docker cp ~/Downloads/lms.env.json edx.devstack.studio:/edx/app/edxapp/
+docker cp ~/Downloads/cms.auth.json edx.devstack.studio:/edx/app/edxapp/
+docker cp ~/Downloads/lms.env.json edx.devstack.studio:/edx/app/edxapp/
 
-docker cp ~/Downloads/lms.auth.json.json edx.devstack.lms:/edx/app/edxapp/
-docker cp ~/Downloads/lms.env.json.json edx.devstack.lms:/edx/app/edxapp/
-docker cp ~/Downloads/cms.auth.json.json edx.devstack.lms:/edx/app/edxapp/
-docker cp ~/Downloads/lms.env.json.json edx.devstack.lms:/edx/app/edxapp/
+docker cp ~/Downloads/lms.auth.json edx.devstack.lms:/edx/app/edxapp/
+docker cp ~/Downloads/lms.env.json edx.devstack.lms:/edx/app/edxapp/
+docker cp ~/Downloads/cms.auth.json edx.devstack.lms:/edx/app/edxapp/
+docker cp ~/Downloads/lms.env.json edx.devstack.lms:/edx/app/edxapp/
